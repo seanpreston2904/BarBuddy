6c530597c80c36958526fdee5390d7d3045aaeeb
Sean <sean.preston2904@gmail.com>
2022-06-18 09:17:20 +1200

    Added linked in link

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -60,7 +60,7 @@ export const Landing:FunctionComponent = () => {
                 }
                 
                 <div className="flex justify-center">
-                    <p className="my-auto text-slate-400">Sean Preston 2022 • <a href="https://github.com/seanpreston2904">GitHub</a> • LinkedIn</p>
+                    <p className="my-auto text-slate-400">Sean Preston 2022 • <a href="https://github.com/seanpreston2904">GitHub</a> • <a href="https://www.linkedin.com/in/sean-preston-2904/">LinkedIn</a></p>
                 </div>
 
             </div>
